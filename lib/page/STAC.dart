1f1a7037a23d7f37bc79b286c0523e297af24967
Chaetenya-Sharma <86671413+Chaetenya-Sharma@users.noreply.github.com>
2022-01-29 22:28:38 +0530

    Add files via upload

---
+++ lib/page/STAC.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PerformancePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('STAC'),
+          centerTitle: true,
+          backgroundColor: Colors.black54,
+        ),
+      );
+}
